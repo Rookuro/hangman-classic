e8e32d66b32194361bb74375f9bad3af81ca4741
Rookuro <alexis.giromagny@ynov.com>
2021-12-08 16:08:14 +0100

    victory.go

--- victory.go
+++ victory.go
@@ -9,19 +9,12 @@ import (
 //Cette fonction permet de vérifier si le mot donner est complet, il affichera en cas de victore deux phrases.
 func Victory() {
 	var compt int
-	var motcomplet string
 	for i := 0; i < len(hidewords); i++ {
-		motcomplet += hidewords[i]
-	}
-	var runemotcomplet = []rune(motcomplet)
-	for i := 0; i < len(runemotcomplet); i++ {
-		if runemotcomplet[i] <= 'z' && runemotcomplet[i] >= 'a' {
-			compt += 1
-		} else {
-			compt += 0
+		if hidewords[i] == tab[i] {
+			compt++
 		}
 	}
-	if compt == len(words) {
+	if compt == len(tab) {
 		fmt.Println("Pal mal, vous m'avez battu ^_^")
 		time.Sleep(2 * time.Second)
 		fmt.Println("On remet en jeu votre titre ?")
